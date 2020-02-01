db56ed4f40bc2a4986b828f3545091a7e3798adf
Cade Anderson <33358801+Edacth@users.noreply.github.com>
2020-02-01 15:22:08 -0800

    Add lotsa scripts

---
+++ Assets/Scripts/Pusher.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pusher : MonoBehaviour
+{
+    public float speed;
+    int tpIndex;
+    public TravelPoint[] travelPoints;
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, travelPoints[tpIndex].pos, speed);
+
+        if (Vector3.Distance(transform.position, travelPoints[tpIndex].pos) < 0.1f)
+        {
+            tpIndex++;
+            if (tpIndex == travelPoints.Length)
+            {
+                tpIndex = 0;
+            }
+        }
+    }
+
+    [System.Serializable]
+    public struct TravelPoint
+    {
+        public Vector3 pos;
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        for (int i = 0; i < travelPoints.Length; i++)
+        {
+            Gizmos.DrawSphere(travelPoints[i].pos, 0.1f);
+        }
+    }
+}
