d8e1b7326c1fa3ee1175d13f0270d5f7b6207d7e
Cade Anderson <33358801+Edacth@users.noreply.github.com>
2020-02-02 13:36:25 -0800

    MenuScene

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public string sceneName;
+
+    void Update()
+    {
+        
+    }
+
+    public void Load()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
