a1740768ad41d6b4d6c2395f610b558c6618bf3e
Cade Anderson <33358801+Edacth@users.noreply.github.com>
2020-02-01 22:25:41 -0800

    Add CatchScript

---
+++ Assets/Scripts/CatchScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatchScript : MonoBehaviour
+{
+    public GameObject subject;
+    public float YLevel;
+    public Vector3 startPoint;
+    GameObject[] collectables;
+    void Start()
+    {
+        collectables = GameObject.FindGameObjectsWithTag("Collectable");
+    }
+
+    void FixedUpdate()
+    {
+        if (subject.transform.position.y < YLevel)
+        {
+            subject.transform.position = startPoint;
+            
+            for (int i = 0; i < collectables.Length; i++)
+            {
+                collectables[i].SetActive(true);            }
+        }
+    }
+}
