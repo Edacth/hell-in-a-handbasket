f57299557ac2b83701762e71c98baebf37d5990c
Cade Anderson <33358801+Edacth@users.noreply.github.com>
2020-02-01 20:57:48 -0800

    SOUNDS

---
+++ Assets/Scripts/AudioScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    public AudioClip[] audioClips;
+    AudioSource AS;
+    void Start()
+    {
+        AS = GetComponent<AudioSource>();
+    }
+
+    public void PlaySlurp()
+    {
+        int soundIndex;
+        soundIndex = Random.Range(0, audioClips.Length);
+
+        AS.PlayOneShot(audioClips[soundIndex]);
+        Debug.Log("Slurp");
+    }
+
+}
